3ebec909f98264f7274bf8f1e4e335f24ad1efa8
Vamsi Kalyan <kalyanvamsi01@gmail.com>
2026-09-15 07:15:00 +0530

    Added Binary Number System with Conversions

--- Code/Number_System_Toolkit.cpp
+++ Code/Number_System_Toolkit.cpp
@@ -1,5 +1,111 @@
 #include<iostream>
+#include<string>
+#include<cmath>
 using namespace std;
+
+class NumberSystem
+{
+   protected:
+      string value;
+      int base;
+      int decimal;
+   public:
+      NumberSystem(int base_val) {
+         base = base_val;
+         decimal = 0;
+      }
+      void GetInput(){
+         cin >> value ;
+      }
+      void display(){
+         cout <<"Value : " << value << endl;
+         cout <<"Base : " << base << endl;
+      }
+      virtual  bool validate() = 0;
+      virtual void convert() = 0;
+};
+
+class BinarySystem : public NumberSystem
+{
+   public :
+      BinarySystem() : NumberSystem(2){ }
+      bool validate(){
+         bool validation = true;
+         for( int i = 0; i < value.length(); i++ ){
+            if( value[i] != '0' && value[i] != '1' ){
+               validation = false;
+               break;
+            }
+         }
+         return validation;
+      }
+      void convert(){
+         char converting_base;
+         cout << "=================================" << endl;
+         cout << "Convert Binary to:" << endl;
+         cout << "1. Decimal\n";
+         cout << "2. Octal\n";
+         cout << "3. Hexadecimal\n";
+         cout << "=================================" << endl;
+         cin >> converting_base;
+         switch( converting_base ){
+            case '1' : {
+               cout << "Converting to Decimal....." << endl;
+               decimal_conversion();
+               cout << "The Decimal value is : " << decimal << endl;
+               break;
+            }
+            case '2' : {
+               decimal_conversion();
+               cout << "Converting to Octal....." << endl;
+               string octal = octal_conversion();
+               cout << "The Octal value is : " << octal << endl;
+               break;
+            }
+            case '3' : {
+               cout << "Coverting to Hexadecimal....." << endl;
+               decimal_conversion();
+               string hex = hex_conversion();
+               cout << "The Hexadecimal value is : " << hex << endl;
+               break;
+            }
+            default : {
+               cout << "Invalid Choice !" << endl;
+               break;
+            }
+         }
+      }
+   private :
+      void decimal_conversion(){
+         decimal = 0;
+         int position = 0;
+         for( int i = value.length() - 1; i >= 0; i-- ){
+            decimal += (value[i] - '0') * pow(2,position);
+            position++;
+         }
+      }
+      string octal_conversion(){
+         string octal = "";
+         while( decimal > 0 ){
+            int remainder = decimal % 8;
+            octal = to_string(remainder) + octal;
+            decimal /= 8;
+         }
+         return octal;
+      }
+      string hex_conversion(){
+         string hex = "";
+         while(decimal > 0){
+            int remainder = decimal % 16;
+            if(remainder < 10)
+               hex = to_string(remainder) + hex;
+            else
+               hex = char('A' + remainder - 10) + hex;
+            decimal /= 16;
+         }
+         return hex;
+      }
+};
 int main(){
     char choice;
     cout << "==============================================" << endl;
